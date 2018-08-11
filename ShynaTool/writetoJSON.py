c0fe37e131d29d0bc07c47b4e9a962411c5919af
shivam623 <shivamsharma1913@gmail.com>
2018-08-12 00:40:52 +0530

    Read this before proceeding.

---
+++ ShynaTool/writetoJSON.py
@@ -0,0 +1,7 @@
+import json
+
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
