350eb947815b18e8795e2b8143387958458b8e7c
shivam623 <shivamsharma1913@gmail.com>
2018-08-11 16:03:26 +0530

    Using google_speech for text to speech

--- Shyna_speech/Shyna_speak.py
+++ Shyna_speech/Shyna_speak.py
@@ -1,1 +1,15 @@
-print ("Hello git")
+from google_speech import Speech
+
+def shyna_speaks(msg):
+    lang = "en"
+    speech = Speech(msg, lang)
+    sox_effects = ("speed", "1", )
+    speech.play(sox_effects)
+
+
+def test_shyna_speaks():
+    text = input("Enter string")
+    shyna_speaks(text)
+
+
+test_shyna_speaks()
