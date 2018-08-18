5c4aec0f7b21938b47bb7dd0a84bf9d13184e5b7
shivam623 <shivamsharma1913@gmail.com>
2018-08-18 16:17:27 +0530

    Offline Shyna Trigger

--- Shyna_speech/Shyna_Pocketsphinx.py
+++ Shyna_speech/Shyna_Pocketsphinx.py
@@ -1,5 +1,6 @@
 from pocketsphinx import LiveSpeech
 from Shyna_speech import  Shyna_speak
+import speech_recognition as sr
 
 
 def shyna_offline():
@@ -8,9 +9,20 @@ def shyna_offline():
         res = str(phrase)
         print(res)
         if res.__contains__("SHYNA"):
-            print ("did you just call me?")
-            res = "did you just call me?"
+            # print("did you just call me?")
+            res = "Shiv!"
             Shyna_speak.shyna_speaks(res)
+            try:
+                r = sr.Recognizer()
+                with sr.Microphone() as source:
+                    print("say Something")
+                    audio = r.listen(source, phrase_time_limit=None)
+                    response = r.recognize_google(audio)
+                    # Shyna_speak.shyna_speaks("Yes Query Is Passing")
+                    print(response)
+                    # return response
+            except sr.UnknownValueError:
+                print("what was that")
 
 
 shyna_offline()
