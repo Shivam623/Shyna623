cbc2a45e7b8e0a30fda0bfc04121f2942049f330
shivam623 <shivamsharma1913@gmail.com>
2018-08-24 05:14:29 +0530

    Shyna speaking each sentence one at a time thanks to NLTK and Python's google_speech module

--- Shyna_speech/Shyna_speak.py
+++ Shyna_speech/Shyna_speak.py
@@ -1,15 +1,20 @@
 from google_speech import Speech
+from nltk.tokenize import sent_tokenize
+import random
 
 
 def shyna_speaks(msg: object) -> object:
-    lang = "en"
-    speech = Speech(msg, lang)
-    sox_effects = ("speed", "1", )
-    speech.play(sox_effects)
+    msgs=random.choice(msg.split("|"))
+    for i in sent_tokenize(msgs):
+        print(i)
+        lang = "en"
+        speech = Speech(i, lang)
+        sox_effects = ("speed", "1", )
+        speech.play(sox_effects)
 
 
 def test_shyna_speaks():
-    text = input("Enter string")
+    text = "I’m good|Pretty good|I’m well|I’m OK|Not too bad|Same old, same old|Yeah, all right|I’m alive!"
     shyna_speaks(text)
 
 
